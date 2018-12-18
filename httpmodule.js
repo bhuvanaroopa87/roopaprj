b43c1021475b7a455e7b1ed12ef3c1c31ab559b5
bhuvanaroopa87 <bhuvana.roopa87@gmail.com>
2018-12-18 22:17:55 +0100

    modified and commit

--- httpmodule.js
+++ httpmodule.js
@@ -1,7 +1,7 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
-if(req.url=='/Home'){
+if(req.url==='/Home'){
     res.write("welcome to ECommerce Application!");
     res.end();
 }
@@ -10,12 +10,14 @@ const data=[
     {name:"rice", price:17, description:"main source to live"},
     {name:"horlicks", price:5, description:"good for childrens growth"}
 ]
-if(req.url=='/products'){
+if(req.url==='/Products'){
     res.write(JSON.stringify(data));
     res.end();
 }
-const userdetails=[username="Roopa" , address="39 Rue de Brement,Noisy Le Sec"]
-if(req.url=='/users'){
+const userdetails=[
+    {username:"Roopa" , address:"39 Rue de Brement,Noisy Le Sec"}
+]
+if(req.url==='/Users'){
     res.write(JSON.stringify(userdetails));
     res.end();
 }
