009aee0507fab4c5ab2020ff4c2c7ff054a9f4ab
bhuvanaroopa87 <bhuvana.roopa87@gmail.com>
2018-12-18 15:47:03 +0100

    commit

---
+++ httpmodule.js
@@ -0,0 +1,24 @@
+const http=require('http');
+
+const server=http.createServer((req,res)=>{
+if(req.url=='/Home'){
+    res.write("welcome to ECommerce Application!");
+    res.end();
+}
+const data=[
+    {name:"babymilk", price:8, description:"used for babies 10months to 3yrs"},
+    {name:"rice", price:17, description:"main source to live"},
+    {name:"horlicks", price:5, description:"good for childrens growth"}
+]
+if(req.url=='/products'){
+    res.write(JSON.stringify(data));
+    res.end();
+}
+const userdetails=[username="Roopa" , address="39 Rue de Brement,Noisy Le Sec"]
+if(req.url=='/users'){
+    res.write(JSON.stringify(userdetails));
+    res.end();
+}
+});
+server.listen("3000");
+console.log("listening to the port :3000");
